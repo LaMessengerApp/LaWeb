396fdb20cf5a378a1eb0eba1e8320672dcb28bf8
Maxime Blondeau <maxime.blondeau92@gmail.com>
2014-12-30 18:03:31 +0100

    1 étape install FOSRestBundle
    1 étape du tuto :
    http://obtao.com/blog/2013/12/creer-une-api-rest-dans-une-application-sy
    mfony/
    Pour l’api Rest
    Faire un composer update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,8 @@ class AppKernel extends Kernel
             new La\AppBundle\LaAppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new La\UserBundle\LaUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
